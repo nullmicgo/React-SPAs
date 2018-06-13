75afaab534303b851d0645804df37d9d038bc95e
nullmicgo <nullmicgo@gmail.com>
2018-06-13 08:11:43 +0800

    Styling Active Links - Changing Home Link with different background colour by adding 'activeStyle'

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => (
   <header>
     <span className="icn-logo"><i className="material-icons">code</i></span>
     <ul className="main-nav">
-      <li><NavLink exact to="/">Home</NavLink></li>
+      <li><NavLink exact to="/" activeStyle={{background:'tomato'}} >Home</NavLink></li>
       <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
       <li><NavLink to="/teachers">Teachers</NavLink></li>
       <li><NavLink to="/courses">Courses</NavLink></li>
