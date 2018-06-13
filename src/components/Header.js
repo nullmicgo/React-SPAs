abf37e74e4ab4d3d63c4b14810b8c129001aa6ad
nullmicgo <nullmicgo@gmail.com>
2018-06-13 08:06:45 +0800

    Adding 'exact' to the Home to prevent 'Home' always active in other section

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => (
   <header>
     <span className="icn-logo"><i className="material-icons">code</i></span>
     <ul className="main-nav">
-      <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink exact to="/">Home</NavLink></li>
       <li><NavLink to="/about">About</NavLink></li>
       <li><NavLink to="/teachers">Teachers</NavLink></li>
       <li><NavLink to="/courses">Courses</NavLink></li>
